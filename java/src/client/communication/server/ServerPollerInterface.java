feb96610a820c77f7bfe097c7e53d588aeaa030c
Kevin Johansen <mrskybear@gmail.com>
2014-09-30 17:31:04 -0600

    First pass at ServerPoller.

--- java/src/client/communication/server/ServerPollerInterface.java
+++ java/src/client/communication/server/ServerPollerInterface.java
@@ -1,11 +1,26 @@
 package client.communication.server;
 
+import client.communication.server.ServerPoller.ModelStateObserver;
+
 /**
  * Communicates with the game server to keep the model state in sync for all players in the game 
  *
  */
 public interface ServerPollerInterface
 {	
+
+	/**
+	 * Begins the polling process
+	 * @param interval Number of milliseconds between each poll to the server
+	 */
+	public void startPoller(long interval);
+	
+	/**
+	 * Adds the observer to the ServerPoller's list of observers. Observers will be notified when the Catan game model changes
+	 * @param new_observer An object that implements the ServerPoller.ModelStateObserver interface. 
+	 */
+	public void registerObserver(ModelStateObserver new_observer);
+	
 	/**
 	 * @return A JSON String representing the current state of all model objects. This object must have a current ServerProxy object set before this method is called.
 	 */
@@ -15,4 +30,5 @@ public interface ServerPollerInterface
 	 * @param proxy The desired end point for all server calls. A ServerProxy object will communicate with the real game server. A ServerMoxy provides hard-coded responses for testing purposes.
 	 */
 	public void setProxy(ServerProxyInterface proxy);
+	
 }
