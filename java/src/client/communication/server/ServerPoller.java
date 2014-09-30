090e185b2cbdbbe2c4d0a4ba8ad7c17cfa790029
Kevin Johansen <mrskybear@gmail.com>
2014-09-30 17:31:49 -0600

    First pass at ServerPoller.

---
+++ java/src/client/communication/server/ServerPoller.java
@@ -0,0 +1,86 @@
+package client.communication.server;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ServerPoller implements ServerPollerInterface
+{
+	private boolean isRunning;
+	private String latestModelVersion;
+	private String gameId;
+	private String latestModel;
+	private Timer pollTimer = null;
+	private TimerTask pollMethod = null;
+	private ServerProxyInterface proxyObject = null;
+	
+	private transient Collection<ModelStateObserver> modelObservers;
+	
+	public ServerPoller(String current_model_version)
+	{
+		isRunning = false;
+		latestModelVersion = current_model_version;
+		
+		boolean is_daemon = true;
+		pollTimer = new Timer(is_daemon);
+		pollMethod = new CatanPoller();
+		
+		modelObservers = new ArrayList<ModelStateObserver>();
+	}
+	
+	public void startPoller(long interval)
+	{
+		if (!isRunning)
+		{
+			Date current_time = new Date();
+			pollTimer.scheduleAtFixedRate(pollMethod, current_time, interval);
+			isRunning = true;
+		}
+	}
+	
+	public void registerObserver(ModelStateObserver new_observer)
+	{
+		modelObservers.add(new_observer);
+	}
+	
+	@Override
+	public String getModelState()
+	{
+		return latestModel;
+	}
+
+	@Override
+	public void setProxy(ServerProxyInterface proxy)
+	{
+		proxyObject = proxy;
+	}
+	
+	private class CatanPoller extends TimerTask
+	{
+		@Override
+		public void run()
+		{
+			//gameId JSON? Where will this come from
+			String model = proxyObject.getGameModel(gameId, latestModelVersion);
+			
+			if (!model.equals("True"))
+			{
+				latestModel = model;
+				
+				//Notify listeners
+				for (ModelStateObserver o : modelObservers)
+				{
+					o.modelChanged(latestModel);
+				}
+				
+			}
+		}
+	}
+	
+	public interface ModelStateObserver
+	{
+		public void modelChanged(String new_model);	
+	}
+}
