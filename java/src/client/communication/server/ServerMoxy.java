729bc8b10a4bd5041878a960cf927e3c104a651c
Kevin Johansen <MrSkyBear@gmail.com>
2014-10-02 22:52:41 -0600

    Updated Moxy and Poller

--- java/src/client/communication/server/ServerMoxy.java
+++ java/src/client/communication/server/ServerMoxy.java
@@ -1,5 +1,10 @@
 package client.communication.server;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ServerMoxy implements ServerProxyInterface
 {
 	
@@ -8,565 +13,177 @@ public class ServerMoxy implements ServerProxyInterface
 
 	}
 	
-	@Override
-	public String login(String JSONString)
+	private String readFile(String filename)
 	{
+		BufferedReader reader = null;
 		StringBuilder builder = new StringBuilder();
-		builder.append("{ \"name\": \"Mario\", \"playerID\": 25 }");
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+			    builder.append(line);
+			}		
+		} 
+		catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				reader.close();
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		return builder.toString();
 	}
 
 	@Override
-	public String register(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("{ \"name\": \"Cookie-Monster\", \"playerID\": 10 }");
-		return builder.toString();
+	public String login(String JSONString) {
+		return "Success";
 	}
 
 	@Override
-	public String listGames()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("[\n");
-		
-		builder.append("{\n");
-		builder.append("\"title\": \"Game 1\",\n");
-		builder.append("\"id\": 1,\n");
-		builder.append("\"players\": ");
-		builder.append("[\n");
-		builder.append("{ \"color\": \"puce\", \"name\": \"Chris\", \"id\": 19},\n");
-		builder.append("{ \"color\": \"orange\", \"name\": \"Mario\", \"id\": 20},\n");
-		builder.append("{ \"color\": \"white\", \"name\": \"Casey\", \"id\": 21},\n");
-		builder.append("{ \"color\": \"green\", \"name\": \"Kevin\", \"id\": 22},\n");
-		builder.append("]\n");
-		builder.append("},\n");
-		
-		builder.append("\"title\": \"Game 2\",\n");
-		builder.append("\"id\": 2,\n");
-		builder.append("\"players\": ");
-		builder.append("[\n");
-		builder.append("{ \"color\": \"red\", \"name\": \"InYang\", \"id\": 123},\n");
-		builder.append("{ \"color\": \"orange\", \"name\": \"Casey\", \"id\": 456},\n");
-		builder.append("{ \"color\": \"white\", \"name\": \"Chris\", \"id\": 789},\n");
-		builder.append("{},\n");
-		builder.append("]\n");
-		builder.append("}\n");
-		
-		builder.append("]");
-		return builder.toString();
+	public String register(String JSONString) {
+		return "Success";
 	}
 
 	@Override
-	public String createGame()
-	{
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("{\n");
-		builder.append("\"title\": \"My Game\",\n");
-		builder.append("\"id\": 1,\n");
-		builder.append("\"players\": ");
-		builder.append("[\n");
-		builder.append("{},\n");
-		builder.append("{},\n");
-		builder.append("{},\n");
-		builder.append("{}\n");
-		builder.append("]\n");
-		builder.append("}");
-		
-		return builder.toString();
+	public String listGames() {
+		return readFile("test/JSON/listGames.txt");
 	}
 
 	@Override
-	public String joinGame(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
+	public String createGame() {
+		return readFile("test/JSON/createGame.txt");
 	}
 
 	@Override
-	public String getGameModel(String JSONString, String latest_model)
-	{
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("{\n");
-		
-		builder.append(buildBank());
-		builder.append(buildChat());
-		builder.append(buildLog());
-		builder.append(buildMap());
-		builder.append(buildPlayers());
-		builder.append(buildTradeOffer());
-		builder.append(buildTurnTracker());
-		builder.append("\"version\": 3,\n");
-		builder.append("\"winner\": -1\n");
-		
-		builder.append("}");
-		
-		return builder.toString();
+	public String joinGame(String JSONString) {
+		return "Success";
+	}
+
+	@Override
+	public String getGameModel(String JSONString, String latest_model) {
+		return readFile("test/JSON/getGameModel.txt");
+	}
+
+	@Override
+	public String resetGame(String JSONString) {
+		return readFile("test/JSON/resetGame.txt");
+	}
+
+	@Override
+	public String getGameActionList(String JSONString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String postGameActionList(String JSONString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAIList() {
+		return "[ \"LARGEST_ARMY\" ]";
+	}
+
+	@Override
+	public String postNewAI(String JSONString) {
+		return "Could not add AI to game.";
+	}
+
+	@Override
+	public String utilChangeLogLevel(String JSONString) {
+		return "Success";
+	}
+
+	@Override
+	public String sendChat(String JSONString) {
+		return readFile("test/JSON/sendChat.txt");
+	}
+
+	@Override
+	public String acceptTrade(String JSONString) {
+		return readFile("test/JSON/acceptTrade.txt");
+	}
+
+	@Override
+	public String discardCards(String JSONString) {
+		return readFile("test/JSON/discardCards.txt");
+	}
+
+	@Override
+	public String rollNumber(String JSONString) {
+		return readFile("test/JSON/rollNumber.txt");
+	}
+
+	@Override
+	public String buildRoad(String JSONString) {
+		return readFile("test/JSON/buildRoad.txt");
+	}
+
+	@Override
+	public String buildSettlement(String JSONString) {
+		return readFile("test/JSON/buildSettlement.txt");
+	}
+
+	@Override
+	public String buildCity(String JSONString) {
+		return readFile("test/JSON/buildCity.txt");
+	}
+
+	@Override
+	public String offerTrade(String JSONString) {
+		return readFile("test/JSON/offerTrade.txt");
+	}
+
+	@Override
+	public String maritimeTrade(String JSONString) {
+		return readFile("test/JSON/maritimeTrade.txt");
+	}
+
+	@Override
+	public String finishTurn(String JSONString) {
+		return readFile("test/JSON/finishTurn.txt");
+	}
+
+	@Override
+	public String buyDevCard(String JSONString) {
+		return readFile("test/JSON/buyDevCard.txt");
+	}
+
+	@Override
+	public String playYearOfPlenty(String JSONString) {
+		return readFile("test/JSON/yearOfPlenty.txt");
+	}
+
+	@Override
+	public String playRoadBuilding(String JSONString) {
+		return readFile("test/JSON/roadBuilding.txt");
+	}
+
+	@Override
+	public String playSoldier(String JSONString) {
+		return readFile("test/JSON/soldier.txt");
+	}
+
+	@Override
+	public String playMonopoly(String JSONString) {
+		return readFile("test/JSON/playMonopoly.txt");
+	}
+
+	@Override
+	public String playMonument(String JSONString) {
+		return readFile("test/JSON/playMonument.txt");
 	}
 	
-	private String buildBank()
-	{
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("\"bank\": ");
-		builder.append("{\n");
-		builder.append("\"brick\": 10,\n");
-		builder.append("\"ore\": 8,\n");
-		builder.append("\"sheep\": 13,\n");
-		builder.append("\"wheat\": 21,\n");
-		builder.append("\"wood\": 7,\n");
-		builder.append("},\n");
-		
-		return builder.toString();	
-	}
 	
-	private String buildChat()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"chat\": ");
-		builder.append("{\n");
-		builder.append(buildChatLines());
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildLogLines()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"lines\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"message\": \"This is a log message\",\n");
-		builder.append("\"source\": \"A player did something\"\n");
-		builder.append("}\n");
-		builder.append("]\n");
-		return builder.toString();	
-	}
-	private String buildChatLines()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"lines\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"message\": \"This is a chat message\",\n");
-		builder.append("\"source\": \"Player 3\"\n");
-		builder.append("}\n");
-		builder.append("]\n");
-		return builder.toString();	
-	}
-	
-	private String buildLog()
-	{
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("\"log\": ");
-		builder.append("{\n");
-		builder.append(buildLogLines());
-		builder.append("},\n");
-		
-		return builder.toString();	
-	}
-	
-	private String buildMap()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"map\": ");
-		builder.append("{\n");
-		builder.append(buildHexes());
-		builder.append(buildPorts());
-		builder.append(buildRoads());
-		builder.append(buildSettlements());
-		builder.append(buildCities());
-		builder.append("\"radius\": 6,\n");
-		builder.append(buildRobber(3,3));
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildHexes()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"hexes\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append(buildLocation(2,3));
-		builder.append("\"resource\": \"wood\",\n");
-		builder.append("\"number\": 5,\n");
-		builder.append("},\n");
-		
-		builder.append("{\n");
-		builder.append(buildLocation(4,5));
-		builder.append("\"resource\": \"sheep\",\n");
-		builder.append("\"number\": 6,\n");
-		builder.append("},\n");
-		
-		builder.append("],\n");
-		return builder.toString();	
-	}
-	
-	private String buildLocation(int x, int y)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"location\": ");
-		builder.append("{\n");
-		builder.append("\"x\": ");
-		builder.append(x);
-		builder.append(",\n");
-		builder.append("\"y\": ");
-		builder.append(y);
-		builder.append("\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildLocationWithDirection(int x, int y, String direction)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"location\": ");
-		builder.append("{\n");
-		builder.append("\"x\": ");
-		builder.append(x);
-		builder.append(",\n");
-		builder.append("\"y\": ");
-		builder.append(y);
-		builder.append(",\n");
-		builder.append("\"direction\": \"");
-		builder.append(direction);
-		builder.append("\"\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildPorts()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"ports\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"resource\": \"wheat\",\n");
-		builder.append(buildLocation(2,1));
-		builder.append("\"direction\": \"North West\",\n");
-		builder.append("\"ratio\": 3\n");
-		builder.append("}\n");
-		builder.append("],\n");
-		return builder.toString();	
-	}
-	
-	private String buildRoads()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"roads\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"owner\": 2,\n");
-		builder.append(buildLocationWithDirection(2,1, "South East"));
-		builder.append("}\n");
-		builder.append("],\n");
-		return builder.toString();	
-	}
-	
-	private String buildSettlements()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"settlements\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"owner\": 3,\n");
-		builder.append(buildLocationWithDirection(5,4, "West"));
-		builder.append("}\n");
-		builder.append("],\n");
-		return builder.toString();	
-	}
-	
-	private String buildCities()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"cities\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"owner\": 2,\n");
-		builder.append(buildLocationWithDirection(2,2, "South West"));
-		builder.append("}\n");
-		builder.append("],\n");
-		return builder.toString();
-	}
-	
-	private String buildRobber(int x, int y)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"robber\": ");
-		builder.append("{\n");
-		builder.append("\"x\": ");
-		builder.append(x);
-		builder.append(",\n");
-		builder.append("\"y\": ");
-		builder.append(y);
-		builder.append("\n");
-		builder.append("}\n");
-		return builder.toString();	
-	}
-	
-	private String buildPlayers()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"players\": ");
-		builder.append("[\n");
-		builder.append("{\n");
-		builder.append("\"cities\": 2,\n");
-		builder.append("\"color\": \"red\",\n");
-		builder.append("\"discarded\": \"true\",\n");
-		builder.append("\"monuments\": 1,\n");
-		builder.append("\"name\": \"George\",\n");
-		builder.append(buildNewDevCards());
-		builder.append(buildOldDevCards());
-		builder.append("\"playerIndex\": 2,\n");
-		builder.append("\"playedDevCard\": \"false\",\n");
-		builder.append("\"playerID\": 5,\n");
-		builder.append(buildResources());
-		builder.append("\"roads\": 6,\n");
-		builder.append("\"settlements\": 4,\n");
-		builder.append("\"soldiers\": 1,\n");
-		builder.append("\"victoryPoints\": 3\n");
-		builder.append("}\n");
-		builder.append("],\n");
-		return builder.toString();	
-	}
-	
-	private String buildNewDevCards()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"newDevCards\": ");
-		builder.append("{\n");
-		builder.append("\"monopoly\": 1,\n");
-		builder.append("\"monument\": 2,\n");
-		builder.append("\"roadBuilding\": 2,\n");
-		builder.append("\"soldier\": 3,\n");
-		builder.append("\"yearOfPlenty\": 1\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildOldDevCards()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"oldDevCards\": ");
-		builder.append("{\n");
-		builder.append("\"monopoly\": 0,\n");
-		builder.append("\"monument\": 2,\n");
-		builder.append("\"roadBuilding\": 2,\n");
-		builder.append("\"soldier\": 3,\n");
-		builder.append("\"yearOfPlenty\": 1\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildResources()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"resources\": ");
-		builder.append("{\n");
-		builder.append("\"brick\": 1,\n");
-		builder.append("\"ore\": 3,\n");
-		builder.append("\"sheep\": 2,\n");
-		builder.append("\"wheat\": 0,\n");
-		builder.append("\"wood\": 4,\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildTradeOffer()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"tradeOffer\": ");
-		builder.append("{\n");
-		builder.append("\"sender\": 1,\n");
-		builder.append("\"receiver\": 3,\n");
-		builder.append("\"offer\": ");
-		builder.append("{\n");
-		builder.append("\"brick\": 0,\n");
-		builder.append("\"ore\": -1,\n");
-		builder.append("\"sheep\": 2,\n");
-		builder.append("\"wheat\": 0,\n");
-		builder.append("\"wood\": 0\n");
-		builder.append("}\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-	
-	private String buildTurnTracker()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("\"turnTracker\": ");
-		builder.append("{\n");
-		builder.append("\"currentTurn\": 1,\n");
-		builder.append("\"status\": \"playing\",\n");
-		builder.append("\"longestRoad\": 1,\n");
-		builder.append("\"largestArmy\": 0\n");
-		builder.append("},\n");
-		return builder.toString();	
-	}
-
-	@Override
-	public String resetGame(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String getGameActionList(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String postGameActionList(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		//returns game model on success
-		return builder.toString();
-	}
-
-	@Override
-	public String getAIList()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("[\"EasyAI\", \"Medium AI\", \"Hard AI\"]");
-		
-		return builder.toString();
-	}
-
-	@Override
-	public String postNewAI(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("");
-		
-		return builder.toString();
-	}
-
-	@Override
-	public String utilChangeLogLevel(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String sendChat(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String acceptTrade(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String discardCards(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String rollNumber(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String buildRoad(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String buildSettlement(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String buildCity(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String offerTrade(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String maritimeTrade(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String finishTurn(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String buyDevCard(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String playYearOfPlenty(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String playRoadBuilding(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String playSoldier(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String playMonopoly(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
-
-	@Override
-	public String playMonument(String JSONString)
-	{
-		StringBuilder builder = new StringBuilder();
-		return builder.toString();
-	}
 
 }
