1a055e7fb60af9d3bfa38a5dc2844b10cb44706d
Chris Belyeu <cbelyeu@verisage.us>
2014-10-02 23:50:39 -0600

    see above

--- java/src/client/model/card/TradeInterface.java
+++ java/src/client/model/card/TradeInterface.java
@@ -4,8 +4,22 @@ import client.model.card.ResourceCardInterface;
 
 public interface TradeInterface {
 	
-	public void setTradeCard(ResourceCardInterface card, int count);
+	/**
+	 * sets the number of a card type that we want to get
+	 * @param card type
+	 * @param count the number of the card that we will give/get
+	 * a positive number means that we will give them the cards
+	 * a negative number means that we expect cards in return
+	 */
+	public void setTradeCard(ResourceCardInterface.ResourceCardType type, int count);
 	
-	public int getTradeCard(ResourceCardInterface card);
+	/**
+	 * gets the number of a give card type that we will be trading
+	 * @param the card type that we are interested in
+	 * @return the number of that card that will be traded
+	 * positive means that you will be giving cards
+	 * negative means that you expect cards in return
+	 */
+	public int getTradeCard(ResourceCardInterface.ResourceCardType type);
 	
 }
