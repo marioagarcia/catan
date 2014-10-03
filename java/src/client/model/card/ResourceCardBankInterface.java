57070878b70a57c48c49511965e7d97bbc05ccda
Chris Belyeu <cbelyeu@verisage.us>
2014-10-02 22:59:15 -0600

    updated the javadoc comments

--- java/src/client/model/card/ResourceCardBankInterface.java
+++ java/src/client/model/card/ResourceCardBankInterface.java
@@ -8,10 +8,8 @@ import client.model.player.PlayerInterface;
 public interface ResourceCardBankInterface
 {
 	/**
-	 * Removes a card from the CardBank and places it in the given Player's CardInventory.
-	 * There must be at least one of the given card type that exists in the CardBank when this method is called. 
-	 * @param recipient The Player receiving the Card from the CardBank
-	 * @param card The Card to be given
+	 * Adds a card to the CardBank 
+	 * @param card. The card being added
 	 */
 	public abstract void addCard(ResourceCardInterface card);
 	
@@ -25,8 +23,7 @@ public interface ResourceCardBankInterface
 	/**
 	 * Removes a card from the Player's CardInventory and places it in the CardBank.
 	 * The Player must have at least one of the given Card objects when this method is called. 
-	 * @param giver The Player giving the Card to the CardBank
-	 * @param card The Card to be taken
+	 * @param type The Card type to be removed
 	 */
 	public abstract ResourceCardInterface removeCard(ResourceCardInterface.ResourceCardType type) throws NoSuchCardException;
 	
