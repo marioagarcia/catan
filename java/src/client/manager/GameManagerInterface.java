c6dc10a97fe1cb28f6d1bddd2c22a34acd7c00c4
Mario Garcia <mario@Marios-MBP-3.app.byu.edu>
2014-10-03 21:15:48 -0600

    more implementation

--- java/src/client/manager/GameManagerInterface.java
+++ java/src/client/manager/GameManagerInterface.java
@@ -255,8 +255,6 @@ public interface GameManagerInterface
 	
 	public PlayerInterface registerPlayer(PlayerInfo playerInfo);
 	
-	public PlayerInterface loginPlayer(String username, String password);
-	
 	public void logoutPlayer();
 	
 }
