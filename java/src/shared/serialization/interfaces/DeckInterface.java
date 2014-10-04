3228aa78b81154c5440e0ff84abdd9c70be17e6a
BaconBits <fscase_seven13@hotmail.com>
2014-10-04 15:50:39 -0600

    Added Serialization Package

---
+++ java/src/shared/serialization/interfaces/DeckInterface.java
@@ -0,0 +1,7 @@
+package shared.serialization.interfaces;
+
+public interface DeckInterface {
+	
+	public abstract void setDeck(int yearOfPlenty, int monopoly, int soldier, int roadBuild, int monument);
+	
+}
