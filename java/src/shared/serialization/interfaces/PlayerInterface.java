5cc94e1b7993383bb9d128ae9f65b65838caaa18
BaconBits <fscase_seven13@hotmail.com>
2014-10-06 08:30:55 -0600

    Update PlayerInterface

--- java/src/shared/serialization/interfaces/PlayerInterface.java
+++ java/src/shared/serialization/interfaces/PlayerInterface.java
@@ -1,11 +1,12 @@
 package shared.serialization.interfaces;
 
 import client.model.card.DevCardList;
+import client.model.card.ResourceList;
 import shared.definitions.CatanColor;
 
 public interface PlayerInterface {
 
-	public abstract void setPlayer(ResourceListInterface resourceList, DevCardList oldDevCards, DevCardList newDevCards,
+	public abstract void setPlayer(ResourceList resourceList, DevCardList oldDevCards, DevCardList newDevCards,
 								   int roads, int cities, int settlements, int soldiers, int victoryPoints, int monuments, 
 								   boolean playedDevCard, boolean discarded, int playerID, int playerIndex, String playerName, 
 								   CatanColor playerColor);
