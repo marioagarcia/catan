fbb214b54513d10079b9032da250b07e89f674f9
Mario Garcia <mario@Marios-MBP-3.app.byu.edu>
2014-10-06 08:54:50 -0600

    more implementation for game, chat, and made some more interfaces

---
+++ java/src/client/logging/chat/Message.java
@@ -0,0 +1,30 @@
+package client.logging.chat;
+
+import shared.serialization.interfaces.SerializerMessageInterface;
+
+public class Message implements MessageInterface, SerializerMessageInterface {
+
+		String content;
+		String playerName;
+	
+		public Message(String msg, String src){
+			content = msg;
+			playerName = src;
+		}
+
+		@Override
+		public void setMessageContent(String content) {
+			this.content = content;
+		}
+
+		@Override
+		public void setPlayerName(String playerName) {
+			this.playerName = playerName;
+		}
+
+		@Override
+		public void setMessage(String playerName, String message) {
+			this.playerName = playerName;
+			this.content = message;
+		}
+}
