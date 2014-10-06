ff98a23c72bf93cf0e64c380a6438f9ba7132692
BaconBits <fscase_seven13@hotmail.com>
2014-10-06 16:50:44 -0600

    Changed SerializeGameInfoInterface to take a list of PlayerInfo

--- java/src/shared/serialization/interfaces/SerializerGameInfoInterface.java
+++ java/src/shared/serialization/interfaces/SerializerGameInfoInterface.java
@@ -2,8 +2,10 @@ package shared.serialization.interfaces;
 
 import java.util.ArrayList;
 
+import client.model.player.PlayerInfo;
+
 public interface SerializerGameInfoInterface {
 
-	public abstract void setGameInfo(String gameTitle, int gameId, ArrayList<SerializerPlayerInterface> playerList);
+	public abstract void setGameInfo(String gameTitle, int gameId, ArrayList<PlayerInfo> playerList);
 	
 }
