48ff14908a89cd52137c4ced5498ddd8cfe47366
Chris Belyeu <cbelyeu@verisage.us>
2014-10-06 16:48:43 -0600

    port

--- java/src/client/model/map/Port.java
+++ java/src/client/model/map/Port.java
@@ -1,45 +1,37 @@
 package client.model.map;
 
 import shared.definitions.ResourceType;
+import shared.locations.EdgeDirection;
+import shared.locations.EdgeLocation;
 import shared.locations.HexLocation;
 import shared.serialization.interfaces.SerializerPortInterface;
 
 public class Port implements SerializerPortInterface {
 
-	private String resource; // If omitted, then it's for any resource, Optional
-	private HexLocation location;
-	private String direction;
+	private ResourceType resource; // If omitted, then it's for any resource, Optional
+	private EdgeLocation location;
 	private int ratio;
 	
-	public Port(String rsrc, HexLocation loc, String dir, int rat){
-		resource = rsrc;
-		location = loc;
-		direction = dir;
-		ratio = rat;
+	public Port(ResourceType resource, EdgeLocation location, int ratio){
+		this.resource = resource;
+		this.location = location;
+		this.ratio = ratio;
 	}
 
-	public String getResource() {
+	public ResourceType getResource() {
 		return resource;
 	}
 
-	public void setResource(String resource) {
+	public void setResource(ResourceType resource) {
 		this.resource = resource;
 	}
 
-	public HexLocation getLocation() {
-		return location;
+	public EdgeLocation getLocation() {
+		return this.location;
 	}
 
-	public void setLocation(HexLocation location) {
+	public void setLocation(EdgeLocation location) {
 		this.location = location;
-	}
-
-	public String getDirection() {
-		return direction;
-	}
-
-	public void setDirection(String direction) {
-		this.direction = direction;
 	}
 
 	public int getRatio() {
@@ -51,9 +43,11 @@ public class Port implements SerializerPortInterface {
 	}
 
 	@Override
-	public void setPort(int ratio, ResourceType resource, String direction,
-			HexLocation hexLocation) {
-		// TODO Auto-generated method stub
+	public void setPort(int ratio, ResourceType resource, EdgeLocation direction) {
+		
+		this.ratio = ratio;
+		this.resource = resource;
+		this.location = direction;
 		
 	}
 	
