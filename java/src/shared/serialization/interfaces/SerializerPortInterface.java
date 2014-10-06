e674eb5a4b19d0a2f06b7e6b57a3985093d90aaf
BaconBits <fscase_seven13@hotmail.com>
2014-10-06 16:48:02 -0600

    Changed SerializePortInterface to take an EdgeLocation

--- java/src/shared/serialization/interfaces/SerializerPortInterface.java
+++ java/src/shared/serialization/interfaces/SerializerPortInterface.java
@@ -1,11 +1,11 @@
 package shared.serialization.interfaces;
 
 import shared.definitions.ResourceType;
-import shared.locations.HexLocation;
+import shared.locations.EdgeLocation;
 
 public interface SerializerPortInterface {
 
 	//Don't do this yet Chris; I'm not sure how to handle a port's direction yet, or where to derive it from
-	public abstract void setPort(int ratio, ResourceType resource, String direction, HexLocation hexLocation);
+	public abstract void setPort(int ratio, ResourceType resource, EdgeLocation location);
 	
 }
