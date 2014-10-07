30b6e693810bbdbf430692c3fad770defad78ecb
MrSkyBear <mrskybear@gmail.com>
2014-10-06 20:35:12 -0600

    Fixed DevCard and DevCard Bank

--- java/src/client/model/card/DevCardBank.java
+++ java/src/client/model/card/DevCardBank.java
@@ -2,43 +2,45 @@ package client.model.card;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import client.manager.interfaces.GMDevCardBankInterface;
+import shared.definitions.DevCardType;
 import shared.serialization.interfaces.SerializerDeckInterface;
 
-public class DevCardBank implements DevCardBankInterface, SerializerDeckInterface {
+public class DevCardBank implements DevCardBankInterface, SerializerDeckInterface, GMDevCardBankInterface {
 
-	private Map<DevCardInterface.DevCardType, Integer> cards;
+	private Map<DevCardType, Integer> cards;
 	
 	public DevCardBank()
 	{
-		this.cards = new HashMap<DevCardInterface.DevCardType, Integer>();
+		this.cards = new HashMap<DevCardType, Integer>();
 	}
 	
-	public DevCardBank(Map<DevCardInterface.DevCardType, Integer> cards){
+	public DevCardBank(Map<DevCardType, Integer> cards){
 		this.cards = cards;
 	}
 
 	@Override
-	public void addCard(DevCardInterface card) {
-		if(this.cards.containsKey(card.getType()))
-			cards.put(card.getType(), cards.get(card.getType()) + 1);
+	public void addCard(DevCardType card) {
+		if(this.cards.containsKey(card))
+			cards.put(card, cards.get(card) + 1);
 		else {
-			cards.put(card.getType(), 1);
+			cards.put(card, 1);
 		}
 		return;
 	}
 
 	@Override
-	public DevCardInterface removeCard(DevCardInterface.DevCardType type) throws NoSuchCardException {
+	public DevCardType removeCard(DevCardType type) throws NoSuchCardException {
 		if(!this.cards.containsKey(type)){
 			throw new NoSuchCardException();
 		}
 		cards.put(type, cards.get(type) - 1);
 		
-		return new DevCard(type);
+		return DevCardType.valueOf(type.toString());
 	}
 
-	@Override
-	public boolean containsCard(DevCardInterface.DevCardType type) {
+	public boolean containsCard(DevCardType type) {
 		return this.cards.containsKey(type);
 	}
 
@@ -46,11 +48,21 @@ public class DevCardBank implements DevCardBankInterface, SerializerDeckInterfac
 	public void setDeck(int yearOfPlenty, int monopoly, int soldier,
 			int roadBuild, int monument) {
 		
-		cards.put(DevCardInterface.DevCardType.YEAR_OF_PLENTY, yearOfPlenty);
-		cards.put(DevCardInterface.DevCardType.MONOPOLY, monopoly);
-		cards.put(DevCardInterface.DevCardType.SOLDIER, soldier);
-		cards.put(DevCardInterface.DevCardType.ROAD_BUILD, roadBuild);
-		cards.put(DevCardInterface.DevCardType.MONUMENT, monument);
+		cards.put(DevCardType.YEAR_OF_PLENTY, yearOfPlenty);
+		cards.put(DevCardType.MONOPOLY, monopoly);
+		cards.put(DevCardType.SOLDIER, soldier);
+		cards.put(DevCardType.ROAD_BUILD, roadBuild);
+		cards.put(DevCardType.MONUMENT, monument);
 	}
 
+	@Override
+	public boolean containsAnyCard() {
+		for (Map.Entry<DevCardType, Integer> card_entry : cards.entrySet()){
+			if (card_entry.getValue() > 0){
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
