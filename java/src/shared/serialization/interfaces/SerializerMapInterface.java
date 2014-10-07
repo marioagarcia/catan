219cfaebf882bd5d01ba863c828aabb96eab7f5a
BaconBits <fscase_seven13@hotmail.com>
2014-10-06 19:52:11 -0600

    Adjusted SerializerMapInterface; changed Interface parameters to object parameters

--- java/src/shared/serialization/interfaces/SerializerMapInterface.java
+++ java/src/shared/serialization/interfaces/SerializerMapInterface.java
@@ -2,12 +2,17 @@ package shared.serialization.interfaces;
 
 import java.util.ArrayList;
 
+import client.model.map.Hex;
+import client.model.map.Port;
+import client.model.piece.City;
+import client.model.piece.Road;
+import client.model.piece.Settlement;
 import shared.locations.HexLocation;
 
 public interface SerializerMapInterface {
 
-	public abstract void setMap(ArrayList<SerializerHexInterface> hexList, ArrayList<SerializerRoadInterface> roadList, 
-								ArrayList<SerializerCityInterface> cityList, ArrayList<SerializerSettlementInterface> settlementList,
-								int radius, ArrayList<SerializerPortInterface> portList, HexLocation robberLocation);
+	public abstract void setMap(ArrayList<Hex> hexList, ArrayList<Road> roadList, 
+								ArrayList<City> cityList, ArrayList<Settlement> settlementList,
+								int radius, ArrayList<Port> portList, HexLocation robberLocation);
 	
 }
