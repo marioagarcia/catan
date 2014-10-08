daf352ff20ada40ad2a44c15850c686347a9d78f
Chris Belyeu <cbelyeu@verisage.us>
2014-10-08 14:36:49 -0600

    added lookup table for finding vertexes adjacent to an edge

---
+++ java/src/client/model/LookupTables/VertexesAdjacentToEdgeLookupResult.java
@@ -0,0 +1,21 @@
+package client.model.LookupTables;
+
+import shared.locations.VertexDirection;
+
+class VertexesAdjacentToEdgeLookupResult {
+	VertexDirection clockwisePreceeding;
+	VertexDirection clockwiseSucceeding;
+	
+	public VertexesAdjacentToEdgeLookupResult(VertexDirection clockwisePreceeding, VertexDirection clockwiseSucceeding){
+		this.clockwisePreceeding = clockwisePreceeding;
+		this.clockwiseSucceeding = clockwiseSucceeding;
+	}
+	
+	public VertexDirection getClockwisePreceeding(){
+		return this.clockwisePreceeding;
+	}
+	
+	public VertexDirection getClockwiseSucceeding(){
+		return this.clockwiseSucceeding;
+	}
+}
