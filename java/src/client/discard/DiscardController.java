311f369870c346ce067262dc85ab97e593d710bb
Chris Belyeu <cbelyeu@verisage.us>
2014-10-19 02:11:29 -0600

    initial implementation of DiscardController

--- java/src/client/discard/DiscardController.java
+++ java/src/client/discard/DiscardController.java
@@ -1,7 +1,15 @@
 package client.discard;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
+
 import shared.definitions.*;
 import client.base.*;
+import client.model.turntracker.TurnTracker;
+import client.model.turntracker.TurntrackerInterface.Status;
+import client.communication.facade.ModelFacade;
 import client.misc.*;
 
 
@@ -11,6 +19,30 @@ import client.misc.*;
 public class DiscardController extends Controller implements IDiscardController {
 
 	private IWaitView waitView;
+	private Map<ResourceType, Integer> amounts;
+	
+	private class DiscardObserver implements Observer{
+		
+		@Override
+		public void update(Observable o, Object arg){
+			TurnTracker turn_tracker = (TurnTracker)arg;
+			
+			if(turn_tracker.getStatus() != Status.DISCARDING){
+				return;
+			}
+			
+			for(ResourceType resource : ResourceType.values()){
+				int number_of_resource = ModelFacade.getInstance(null).getManager().getResCardBank().getNumberOfResourcesByType(resource);
+				
+				getDiscardView().setResourceMaxAmount(resource, number_of_resource);
+				getDiscardView().setResourceDiscardAmount(resource, 0);
+				amounts.put(resource,  0);
+			}
+
+			updateDiscardView();
+			getDiscardView().showModal();
+		}
+	}
 	
 	/**
 	 * DiscardController constructor
@@ -21,8 +53,10 @@ public class DiscardController extends Controller implements IDiscardController 
 	public DiscardController(IDiscardView view, IWaitView waitView) {
 		
 		super(view);
+		amounts = new HashMap<ResourceType, Integer>();
 		
 		this.waitView = waitView;
+		ModelFacade.getInstance(null).getManager().getTurnTracker().addObserver(new DiscardObserver());
 	}
 
 	public IDiscardView getDiscardView() {
@@ -35,18 +69,39 @@ public class DiscardController extends Controller implements IDiscardController 
 
 	@Override
 	public void increaseAmount(ResourceType resource) {
-		
+		this.amounts.put(resource, this.amounts.get(resource) + 1);
+		updateDiscardView();
 	}
 
 	@Override
 	public void decreaseAmount(ResourceType resource) {
-		
+		this.amounts.put(resource, this.amounts.get(resource) - 1);
+		updateDiscardView();
 	}
 
 	@Override
 	public void discard() {
-		
 		getDiscardView().closeModal();
+	}
+	
+	private void updateDiscardView(){
+		int total_cards = 0;
+		int total_discard_amount = 0;
+		
+		for(ResourceType resource : ResourceType.values()){
+			int number_of_resource = ModelFacade.getInstance(null).getManager().getResCardBank().getNumberOfResourcesByType(resource);
+			int number_of_resource_to_discard = this.amounts.get(resource);
+			
+			total_cards += number_of_resource;
+			total_discard_amount += number_of_resource_to_discard;
+			getDiscardView().setResourceDiscardAmount(resource, number_of_resource_to_discard);
+			
+			boolean increase = this.amounts.get(resource) < ModelFacade.getInstance(null).getManager().getResCardBank().getNumberOfResourcesByType(resource);
+			boolean decrease = this.amounts.get(resource) > 0;
+			getDiscardView().setResourceAmountChangeEnabled(resource, increase, decrease);
+		}
+		
+		getDiscardView().setStateMessage(total_discard_amount + "/" + (total_cards - 7));
 	}
 
 }
