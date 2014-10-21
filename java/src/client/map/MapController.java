e383533c4c6c41ce612442f62e5c8ca1b96613d7
Kevin Johansen <mrskybear@gmail.com>
2014-10-21 02:39:35 -0600

    Fixed null pointer issue when drawing ports

--- java/src/client/map/MapController.java
+++ java/src/client/map/MapController.java
@@ -46,8 +46,6 @@ public class MapController extends Controller implements IMapController {
 		map.addObserver(m);
 		
 		setRobView(robView);
-		
-		System.out.println("Map controller created");
 	}
 	
 	private class TrackerObserver implements Observer{
@@ -55,7 +53,6 @@ public class MapController extends Controller implements IMapController {
 		@Override
 		public void update(Observable o, Object arg){
 			tracker = (TurnTracker)o;
-			System.out.println("Turn Tracker updated");
 			
 			if (ModelFacade.getInstance(null).getManager().isLocalPlayersTurn()){
 				
@@ -97,7 +94,6 @@ public class MapController extends Controller implements IMapController {
 		public void update(Observable o, Object arg){
 			map = (BoardMap)o;
 			
-			System.out.println("Map updated");
 			initFromModel(map);
 		}
 	}
@@ -149,81 +145,18 @@ public class MapController extends Controller implements IMapController {
 		
 		//Draw ports
 		for (Map.Entry<EdgeLocation, Port> port : m.getPorts().entrySet()){
-		//	Port contains ResourceType, but the view wants a PortType	
-		//	getView().addPort(port.getKey(), port.getValue().getResource());
+
+			if (port.getValue().getResource() != null){
+				getView().addPort(port.getKey(), PortType.valueOf(port.getValue().getResource().toString()));
+			}
+			else
+			{
+				getView().addPort(port.getKey(), PortType.THREE);
+			}
 		}
 		
 		//Place robber
 		getView().placeRobber(m.getRobberLocation());
-		
-		
-		
-		
-		
-		/*
-		
-		Random rand = new Random();
-
-		for (int x = 0; x <= 3; ++x) {
-			
-			int maxY = 3 - x;			
-			for (int y = -3; y <= maxY; ++y) {				
-				int r = rand.nextInt(HexType.values().length);
-				HexType hexType = HexType.values()[r];
-				HexLocation hexLoc = new HexLocation(x, y);
-				getView().addHex(hexLoc, hexType);
-				getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
-						CatanColor.RED);
-				getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
-						CatanColor.BLUE);
-				getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.South),
-						CatanColor.ORANGE);
-				getView().placeSettlement(new VertexLocation(hexLoc,  VertexDirection.NorthWest), CatanColor.GREEN);
-				getView().placeCity(new VertexLocation(hexLoc,  VertexDirection.NorthEast), CatanColor.PURPLE);
-			}
-			
-			if (x != 0) {
-				int minY = x - 3;
-				for (int y = minY; y <= 3; ++y) {
-					int r = rand.nextInt(HexType.values().length);
-					HexType hexType = HexType.values()[r];
-					HexLocation hexLoc = new HexLocation(-x, y);
-					getView().addHex(hexLoc, hexType);
-					getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
-							CatanColor.RED);
-					getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
-							CatanColor.BLUE);
-					getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.South),
-							CatanColor.ORANGE);
-					getView().placeSettlement(new VertexLocation(hexLoc,  VertexDirection.NorthWest), CatanColor.GREEN);
-					getView().placeCity(new VertexLocation(hexLoc,  VertexDirection.NorthEast), CatanColor.PURPLE);
-				}
-			}
-		}
-		
-		PortType portType = PortType.BRICK;
-		getView().addPort(new EdgeLocation(new HexLocation(0, 3), EdgeDirection.North), portType);
-		getView().addPort(new EdgeLocation(new HexLocation(0, -3), EdgeDirection.South), portType);
-		getView().addPort(new EdgeLocation(new HexLocation(-3, 3), EdgeDirection.NorthEast), portType);
-		getView().addPort(new EdgeLocation(new HexLocation(-3, 0), EdgeDirection.SouthEast), portType);
-		getView().addPort(new EdgeLocation(new HexLocation(3, -3), EdgeDirection.SouthWest), portType);
-		getView().addPort(new EdgeLocation(new HexLocation(3, 0), EdgeDirection.NorthWest), portType);
-		
-		getView().placeRobber(new HexLocation(0, 0));
-		
-		getView().addNumber(new HexLocation(-2, 0), 2);
-		getView().addNumber(new HexLocation(-2, 1), 3);
-		getView().addNumber(new HexLocation(-2, 2), 4);
-		getView().addNumber(new HexLocation(-1, 0), 5);
-		getView().addNumber(new HexLocation(-1, 1), 6);
-		getView().addNumber(new HexLocation(1, -1), 8);
-		getView().addNumber(new HexLocation(1, 0), 9);
-		getView().addNumber(new HexLocation(2, -2), 10);
-		getView().addNumber(new HexLocation(2, -1), 11);
-		getView().addNumber(new HexLocation(2, 0), 12);
-		
-		//</temp>
-		 */
 	}
 
 	public boolean canPlaceRoad(EdgeLocation edgeLoc) {
