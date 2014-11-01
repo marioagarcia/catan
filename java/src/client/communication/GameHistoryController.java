64fa3de0bfbdacef19158b3a68b1b21062ab4ea8
Chris Belyeu <cbelyeu@verisage.us>
2014-11-01 15:37:25 -0600

    updated GameHistoryController to work with the new Observable pattern

--- java/src/client/communication/GameHistoryController.java
+++ java/src/client/communication/GameHistoryController.java
@@ -4,10 +4,9 @@ import java.util.*;
 
 import client.base.*;
 import client.communication.facade.ModelFacade;
-import client.model.logging.GameLog;
+import client.model.GameModel;
 import client.model.logging.history.LogLineInterface;
 import client.model.player.Player;
-import client.model.player.Players;
 import shared.definitions.*;
 
 
@@ -16,12 +15,11 @@ import shared.definitions.*;
  */
 public class GameHistoryController extends Controller implements IGameHistoryController {
 
-	private class GameLogObserver implements Observer{
+	private class GameModelObserver implements Observer{
 
 		@Override
 		public void update(Observable o, Object arg){
-			GameLog game_log = (GameLog)o;
-			GameHistoryController.this.update(game_log);
+			GameHistoryController.this.update((GameModel)o);
 		}
 		
 	}
@@ -30,7 +28,7 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 		
 		super(view);
 		
-		ModelFacade.getInstance(null).getManager().getGameLog().addObserver(new GameLogObserver());
+		ModelFacade.getInstance(null).addObserver(new GameModelObserver());
 	}
 	
 	@Override
@@ -38,18 +36,16 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 		return (IGameHistoryView)super.getView();
 	}
 
-	private void update(GameLog game_log){
-		Players players = ModelFacade.getInstance(null).getManager().getAllPlayers();
-		
+	private void update(GameModel game_manager){
 		List<LogEntry> entries = new ArrayList<LogEntry>();
 		
-		for(int i = 0; i < game_log.getGameHistoryLog().getSize(); i++){
+		for(int i = 0; i < game_manager.getGameLog().getGameHistoryLog().getSize(); i++){
 			
-			LogLineInterface line = game_log.getGameHistoryLog().getLogLine(i);
+			LogLineInterface line = game_manager.getGameLog().getGameHistoryLog().getLogLine(i);
 			
 			CatanColor playerColor = CatanColor.WHITE;
 			
-			for(Player player : players.getPlayerList()){
+			for(Player player : game_manager.getPlayers().getPlayerList()){
 				if(player.getName().equals(line.getPlayerName())){
 					playerColor = player.getColor();
 				}
