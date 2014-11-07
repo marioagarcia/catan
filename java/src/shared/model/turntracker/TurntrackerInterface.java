04223a9219a93962789f660db31e71e6cbea0dbd
MrSkyBear <mrskybear@gmail.com>
2014-11-07 15:48:31 -0700

    Javadoc'd the turn tracker interface

--- java/src/shared/model/turntracker/TurntrackerInterface.java
+++ java/src/shared/model/turntracker/TurntrackerInterface.java
@@ -46,6 +46,34 @@ public interface TurntrackerInterface {
 	 */
 	public int getLargestArmy();
 	
+	/**
+	 * Verifies that the given player can roll the dice
+	 * @param player_index
+	 * @return True if it is the player's turn, and the game is in the rolling phase. Otherwise, returns false.
+	 */
+	public boolean canRoll(int player_index);
+
+	/**
+	 * Checks whether the given player is allowed to buy a dev card
+	 * @param player_index The index of a player in the current game
+	 * @return True if it is the current player's turn and the game is in the playing phase. False otherwise
+	 */
+	public boolean canBuyDevCard(int player_index);
+
+	/**
+	 * Checks to see if the given player can play a development card
+	 * @param player_index The index of the player in the current game
+	 * @return True if it is the player's turn and if the game is in the playing phase. False otherwise.
+	 */
+	public boolean canPlayDevCard(int player_index);
+	
+	/**
+	 * Verifies that the current player can make a move
+	 * @param player_index The player busting a move
+	 * @return True if it is the current player's turn and the game is in the playing phase
+	 */
+	public boolean canMakeMove(int player_index);
+	
 	
 	
 	public enum Status{
