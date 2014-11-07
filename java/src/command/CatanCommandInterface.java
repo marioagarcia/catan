e42ddec81e9ba0e5e51088d458819993c23881ea
MrSkyBear <mrskybear@gmail.com>
2014-11-06 20:01:54 -0700

    Added command interface

---
+++ java/src/command/CatanCommandInterface.java
@@ -0,0 +1,11 @@
+package command;
+
+import shared.serialization.parameters.MasterParameterInterface;
+
+public interface CatanCommandInterface{
+	
+	//Should these return 'object' to be cast back on the other end? Or maybe json depending where there serializing happens?
+	public void execute(MasterParameterInterface parameter_package);
+	
+	public boolean wasSuccessful();
+}
