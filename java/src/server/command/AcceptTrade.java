7330858b3bfcd793fef757e495318e267a564068
Kevin Johansen <mrskybear@gmail.com>
2014-11-07 11:35:34 -0700

    AcceptTrade and BuildCity javadocs

--- java/src/server/command/AcceptTrade.java
+++ java/src/server/command/AcceptTrade.java
@@ -2,15 +2,28 @@ package server.command;
 
 import shared.serialization.parameters.AcceptTradeParameters;
 
+/**
+ * A Command object that updates a game to reflect that a user has accepted or rejected a trade offer
+ * @author Kevin
+ */
 public class AcceptTrade extends CatanCommand {
 
-	public AcceptTrade(AcceptTradeParameters parameters){
+	/**
+	 * Initializes the AcceptTrade object with all the information it needs to apply changes to the model
+	 * to reflect the results of a trade offer
+	 * @param parameters An object containing information about whether the trade will be accepted, and the index of the player accepting the trade
+	 * @param game_id The integer ID of the game the operation is to be performed on. Must be an ID for an existing game
+	 */
+	public AcceptTrade(AcceptTradeParameters parameters, int game_id){
 		
 	}
 	
+	/**
+	 * Ensures that the move, given the current parameters, is valid. After calling this method, the players involved in the trade will
+	 * have their resources updated to reflect the trade.
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
 }
--- java/src/server/command/BuildCity.java
+++ java/src/server/command/BuildCity.java
@@ -2,15 +2,27 @@ package server.command;
 
 import shared.serialization.parameters.BuildCityParameters;
 
+/**
+ * A Command object that updates the game to reflect that a player has built a city
+ * @author Kevin
+ */
 public class BuildCity extends CatanCommand {
 
-	public BuildCity(BuildCityParameters parameters){
+	/**
+	 * Initializes the BuildCity object with the information it needs to make changes to the model
+	 * @param parameters An object containing the player performing the move, and the location to place the new city 
+	 * @param game_id The integer ID of the game the operation is to be performed on. Must be an ID for an existing game
+	 */
+	public BuildCity(BuildCityParameters parameters, int game_id){
 		
 	}
 	
+	/**
+	 * If the move is valid, places a city on the map and updates the player's resources, piece count, and victory points.
+	 * After this method is called, the player will have 3 less wheat, 2 less ore, 1 more settlement, 1 less city, and 1 more victory point.
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
 }
