cfbdd856b9a10fbe40417e23680721e0af53ad09
Kevin Johansen <mrskybear@gmail.com>
2014-11-07 11:07:47 -0700

    Added game_id parameter to getGameModel on ModelCommunicator

--- java/src/server/ModelCommunicatorInterface.java
+++ java/src/server/ModelCommunicatorInterface.java
@@ -3,9 +3,26 @@ package server;
 import shared.model.manager.GameData;
 import shared.model.manager.GameList;
 
+/**
+ * The ModelCommunicator provides a way for the Handler classes to get physical data for each Catan game
+ * currently running on the server
+ * @author Kevin
+ */
 public interface ModelCommunicatorInterface {
 
-	public GameData getGameModel();
+	/**
+	 * @param game_id The integer representing the game you wish to get the model for
+	 * @return A {@link shared.model.manager.GameData} object with all the current information about the state of a particular game
+	 */
+	public GameData getGameModel(int game_id);
+	
+	/**
+	 * @return A {@link shared.model.manager.GameList} object containing information about the games running on the server
+	 */
 	public GameList getGameList();
+	
+	/**
+	 * @return An array of strings representing the types of AI available to put in a game
+	 */
 	public String[] getAIList();
 }
