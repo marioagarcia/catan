4809c841b496b7ed4ddab4e78e8ef714f18cc58d
MrSkyBear <mrskybear@gmail.com>
2014-11-06 20:51:27 -0700

    Updated command package

--- java/src/server/command/CatanCommand.java
+++ java/src/server/command/CatanCommand.java
@@ -3,6 +3,8 @@ package server.command;
 import shared.serialization.parameters.MasterParameterInterface;
 
 public abstract class CatanCommand implements CatanCommandInterface {
+	
+	protected boolean success;
 
 	@Override
 	public void execute(MasterParameterInterface parameter_package) {
@@ -10,10 +12,13 @@ public abstract class CatanCommand implements CatanCommandInterface {
 
 	}
 
+	protected void setSuccess(boolean success){
+		this.success = success;
+	}
+	
 	@Override
 	public boolean wasSuccessful() {
-		// TODO Auto-generated method stub
-		return false;
+		return success;
 	}
 
 }
