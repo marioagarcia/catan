0905dd35c2644c67d9ad547e17380c854985877a
Chris Belyeu <cbelyeu@verisage.us>
2014-11-14 18:28:29 -0700

    implemented edgeLocationtest

--- java/src/test/shared/locations/EdgeLocationTest.java
+++ java/src/test/shared/locations/EdgeLocationTest.java
@@ -7,96 +7,57 @@ import org.junit.Test;
 import shared.locations.EdgeDirection;
 import shared.locations.EdgeLocation;
 import shared.locations.HexLocation;
+import shared.model.map.BoardMap;
 
 public class EdgeLocationTest {
 
 	@Test
 	public void testGetAdjacent() {
-//		HexLocation hexLocation = new HexLocation(0,0);
-//
-//		//0,0 NorthEast
-//		EdgeLocation edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.NorthEast);
-//		
-//		EdgeLocation[] result1 = edgeLocation.getAdjacent(true, true);
-//		
-//		assertEquals(4, result1.length);
-//		assertEquals(0, result1[0].getHexLoc().getX());
-//		assertEquals(0, result1[0].getHexLoc().getY());
-//		assertEquals(EdgeDirection.North, result1[0].getDir());
-//		
-//		assertEquals(0, result1[1].getHexLoc().getX());
-//		assertEquals(0, result1[1].getHexLoc().getY());
-//		assertEquals(EdgeDirection.SouthEast, result1[1].getDir());
-//		
-//		assertEquals(1, result1[2].getHexLoc().getX());
-//		assertEquals(-1, result1[2].getHexLoc().getY());
-//		assertEquals(EdgeDirection.South, result1[2].getDir());
-//		
-//		assertEquals(1, result1[3].getHexLoc().getX());
-//		assertEquals(-1, result1[3].getHexLoc().getY());
-//		assertEquals(EdgeDirection.NorthWest, result1[3].getDir());
-//		
-//		//0,2 South
-//		hexLocation = new HexLocation(0,2);
-//		edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.South);
-//		
-//		EdgeLocation[] result2 = edgeLocation.getAdjacent(true, true);
-//		
-//		assertEquals(2, result2.length);
-//		assertEquals(0, result2[0].getHexLoc().getX());
-//		assertEquals(2, result2[0].getHexLoc().getY());
-//		assertEquals(EdgeDirection.SouthEast, result2[0].getDir());
-//		
-//		assertEquals(0, result2[1].getHexLoc().getX());
-//		assertEquals(2, result2[1].getHexLoc().getY());
-//		assertEquals(EdgeDirection.SouthWest, result2[1].getDir());
-//		
-//		//0,2 NorthWest
-//		hexLocation = new HexLocation(-2,2);
-//		edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.NorthWest);
-//		
-//		EdgeLocation[] result3 = edgeLocation.getAdjacent(true, true);
-//		
-//		assertEquals(2, result3.length);
-//		assertEquals(-2, result3[0].getHexLoc().getX());
-//		assertEquals(2, result3[0].getHexLoc().getY());
-//		assertEquals(EdgeDirection.SouthWest, result3[0].getDir());
-//		
-//		assertEquals(-2, result3[1].getHexLoc().getX());
-//		assertEquals(2, result3[1].getHexLoc().getY());
-//		assertEquals(EdgeDirection.North, result3[1].getDir());
-//		
-//		
-//		
-//		//test removing one side (
-//		hexLocation = new HexLocation(0,0);
-//		edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.NorthEast);
-//		
-//		
-//		EdgeLocation[] result4 = edgeLocation.getAdjacent(false, true);
-//		
-////		assertEquals(2, result4.length);
-////		assertEquals(0, result4[0].getHexLoc().getX());
-////		assertEquals(0, result4[0].getHexLoc().getY());
-////		assertEquals(EdgeDirection.North, result4[0].getDir());
-////		
-////		assertEquals(0, result4[1].getHexLoc().getX());
-////		assertEquals(0, result4[1].getHexLoc().getY());
-////		assertEquals(EdgeDirection.SouthEast, result4[1].getDir());
-////		
-////		assertEquals(1, result4[2].getHexLoc().getX());
-////		assertEquals(-1, result4[2].getHexLoc().getY());
-////		assertEquals(EdgeDirection.South, result4[2].getDir());
-////		
-////		assertEquals(1, result4[3].getHexLoc().getX());
-////		assertEquals(-1, result4[3].getHexLoc().getY());
-////		assertEquals(EdgeDirection.NorthWest, result4[3].getDir());	
-//		
-//		
-//		
-//		for(EdgeLocation location : result4){
-//			System.out.println(location);
-//		}
+		HexLocation hexLocation = new HexLocation(0,0);
+
+		//0,0 NorthEast
+		EdgeLocation edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.NorthEast);
+        BoardMap map = new BoardMap(true, true, true);
+
+		EdgeLocation[] result1 = edgeLocation.getAdjacent(true, true, map);
+
+        assertEquals(4, result1.length);
+        assertEquals(new EdgeLocation(new HexLocation(0,0), EdgeDirection.North), result1[0]);
+        assertEquals(new EdgeLocation(new HexLocation(1,0), EdgeDirection.NorthWest), result1[1]);
+        assertEquals(new EdgeLocation(new HexLocation(1,0), EdgeDirection.North), result1[2]);
+        assertEquals(new EdgeLocation(new HexLocation(1,-1), EdgeDirection.NorthWest), result1[3]);
+
+		//0,2 South
+		hexLocation = new HexLocation(0,2);
+		edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.South);
+
+		EdgeLocation[] result2 = edgeLocation.getAdjacent(true, true, map);
+
+		assertEquals(2, result2.length);
+        assertEquals(new EdgeLocation(new HexLocation(1,2), EdgeDirection.NorthWest), result2[0]);
+        assertEquals(new EdgeLocation(new HexLocation(-1,3), EdgeDirection.NorthEast), result2[1]);
+
+		//0,2 NorthWest
+		hexLocation = new HexLocation(-2,2);
+		edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.NorthWest);
+
+		EdgeLocation[] result3 = edgeLocation.getAdjacent(true, true, map);
+
+		assertEquals(3, result3.length);
+        assertEquals(new EdgeLocation(new HexLocation(-3,3), EdgeDirection.NorthEast), result3[0]);
+        assertEquals(new EdgeLocation(new HexLocation(-2,2), EdgeDirection.North), result3[1]);
+        assertEquals(new EdgeLocation(new HexLocation(-3,2), EdgeDirection.NorthEast), result3[2]);
+
+		//test removing one side (
+		hexLocation = new HexLocation(0,0);
+		edgeLocation = new EdgeLocation(hexLocation, EdgeDirection.NorthEast);
+
+
+		EdgeLocation[] result4 = edgeLocation.getAdjacent(false, true, map);
+
+		assertEquals(2, result4.length);
+        assertEquals(new EdgeLocation(new HexLocation(1,0), EdgeDirection.NorthWest), result4[0]);
+        assertEquals(new EdgeLocation(new HexLocation(1,0), EdgeDirection.North), result4[1]);
 	}
 
 }
