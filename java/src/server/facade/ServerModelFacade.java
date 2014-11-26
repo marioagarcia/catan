76e910fe1ce8f0afa338830f81c334574228e77a
MrSkyBear <MrSkyBear@gmail.com>
2014-11-25 21:36:16 -0700

    Updating save/load IDs

--- java/src/server/facade/ServerModelFacade.java
+++ java/src/server/facade/ServerModelFacade.java
@@ -215,7 +215,7 @@ public class ServerModelFacade implements ServerModelFacadeInterface {
 	        
 	        new_game = (ServerGameManager) in.readObject();
 	        
-	   /*     
+	        
 	        boolean exists = false;
 	        
 	        for (Map.Entry<Integer, ServerGameManager> game : gamesList.entrySet()){
@@ -226,10 +226,14 @@ public class ServerModelFacade implements ServerModelFacadeInterface {
 				}	
 			}
 	                
-	        if (!exists){*/
-				new_game.setGameId(currentGameId++);
+	        if (exists){
+				//new_game.setGameId(currentGameId++);
 				gamesList.put(new_game.getGameId(), new_game);
-	   //     }
+	        }
+	        else{
+	        	new_game.setGameId(currentGameId++);
+				gamesList.put(new_game.getGameId(), new_game);
+	        }
 			
 			in.close();
 	        fileIn.close();
