de0e4bba2b0f5bea87c17b7ec5f370d37f1c02e8
Kevin Johansen <mrskybear@gmail.com>
2014-12-03 10:47:45 -0700

    Javadocs for Factory interface

--- java/src/server/persistence/DatabaseAccessFactoryInterface.java
+++ java/src/server/persistence/DatabaseAccessFactoryInterface.java
@@ -1,8 +1,26 @@
 package server.persistence;
 
+/**
+ * An implementation of this interface will be used to create objects that will persist Catan data into a database.
+ * The implementation of the factory will be given to a persistence class which use internally the provided DAOs 
+ */
 public interface DatabaseAccessFactoryInterface {
 
+	/**
+	 * 
+	 * @return An object that will be used to save CatanCommands to the database 
+	 */
 	public abstract CommandDAOInterface createCommandDAO();
+	
+	/**
+	 *
+	 * @return An object that will be used to save Users to the database
+	 */
 	public abstract UserDAOInterface createUserDAO();
+	
+	/**
+	 * 
+	 * @return An object that will be used to save games to the database
+	 */
 	public abstract GameDAOInterface createGameDAO();
 }
